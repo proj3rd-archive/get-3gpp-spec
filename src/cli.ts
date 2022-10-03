d4095b43843ba7f60e4f803e4f7b306eea9a3e2a
Seokseong Jeon <seokseong@jeon.engineer>
2022-10-03 23:48:16 +0900

    download a requested spec

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,5 @@
 import { Client } from "basic-ftp";
+import { parse } from "path";
 import { argv } from "process";
 
 if (require.main === module) {
@@ -16,18 +17,75 @@ async function cli(spec: string, rel: string, quarter: string) {
   const series = getSeries(spec);
   const path = `/Specs/archive/${series}_series/${spec}`;
   await client.cd(path);
-  const fileInfoList = (await client.list()).map((fileInfo) => ({
-    ...fileInfo,
-    date: parseDate(fileInfo.rawModifiedAt),
-  }));
+  const fileInfoList = (await client.list())
+    .filter((fileInfo) => {
+      // Check release verison
+      const { name } = parse(fileInfo.name);
+      const indexHyphen = name.lastIndexOf("-");
+      if (indexHyphen === -1) {
+        throw Error(
+          "Spec must be in a form of AB.CDE[-F]-xyz or AB.CDE[-F]-uvwxyz"
+        );
+      }
+      const version = name.substring(indexHyphen + 1);
+      const release = getRelease(version);
+      if (release !== Number(rel)) {
+        return false;
+      }
+      // Check date
+      const date = parseDate(fileInfo.rawModifiedAt).getTime();
+      const [yy, mm] = quarter.split("-").map(Number);
+      const dateQuarter = new Date(yy, mm - 1).getTime();
+      const dateQuarterPlus3Months = new Date(yy, mm + 2).getTime();
+      return date >= dateQuarter && date < dateQuarterPlus3Months;
+    })
+    .map((fileInfo) => ({
+      ...fileInfo,
+      date: parseDate(fileInfo.rawModifiedAt),
+    }))
+    .sort((a, b) => {
+      return b.date.getTime() - a.date.getTime();
+    });
   console.table(fileInfoList);
+  const latest = fileInfoList[0];
+  await client.downloadTo(latest.name, `${path}/${latest.name}`);
   client.close();
+}
+
+/**
+ * Get a release from version strnig
+ * @param version String in a form of xyz or uvwxyz
+ * @returns Release
+ */
+function getRelease(version: string) {
+  if (version.length === 6) {
+    return Number(version.substring(0, 2));
+  }
+  const map = Object.fromEntries(
+    "123456789abcdefghijklmnopqrstuvwxyz"
+      .split("")
+      .map((char, index) => [char, index + 1])
+  );
+  return map[version[0]];
+}
+
+/**
+ * Get a series number from spec string
+ * @param spec String in a form of ab.cde[-f]
+ * @returns Series number (zero padded, if required)
+ */
+function getSeries(spec: string) {
+  const indexDot = spec.indexOf(".");
+  if (indexDot === -1) {
+    throw Error("Spec must be in a form of ab.cde[-f]");
+  }
+  return spec.substring(0, indexDot);
 }
 
 /**
  * Parse a date string
  * @param date String in a form of `MM-DD-YY HH:mm(AM|PM)`
- * @returns  Date object
+ * @returns Date object
  */
 function parseDate(date: string) {
   const [YYMMDD, hhmmampm] = date.split(" ");
@@ -40,12 +98,4 @@ function parseDate(date: string) {
   const ampm = hhmmampm.substring(hhmmampm.length - 2);
   const hour = ampm === "PM" ? hh + 12 : hh;
   return new Date(year, MM - 1, DD, hour, mm);
-}
-
-function getSeries(spec: string) {
-  const indexDot = spec.indexOf(".");
-  if (indexDot === -1) {
-    throw Error("Spec must be in a form of AB.CDE[-F]");
-  }
-  return spec.substring(0, indexDot);
 }
